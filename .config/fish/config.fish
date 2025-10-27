3ed1a11659d8969c0398d6d49b10321d19284267
StapleTT <talankm@icloud.com>
2025-10-27 08:24:39 -0500

    Fix sway wallpaper and fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+set -g fish_greeting ""
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias history="history -r"
+alias vim="nvim"
+alias mksh="~/importantscripts/mksh.sh"
+alias rm="~/importantscripts/logremove.sh"
+alias ls="eza --icons"
+
+# starship init fish | source
+source (/usr/local/bin/starship init fish --print-full-init | psub)
