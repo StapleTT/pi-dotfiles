1bc9707528b92815d9ee1eebe45a75322f05017f
StapleTT <talankm@icloud.com>
2025-11-18 08:50:39 -0600

    disable smear in insert mode

--- .config/nvim/lua/plugins/smear-cursor.lua
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -2,6 +2,9 @@ return {
   "sphamba/smear-cursor.nvim",
   opts = {
     stiffness = 1.0,
-    trailing_stiffness = 0.4
+    trailing_stiffness = 0.4,
+    legacy_computing_symbols_support = true,
+    scroll_buffer_space = true,
+    smear_insert_mode = false
   },
 }
